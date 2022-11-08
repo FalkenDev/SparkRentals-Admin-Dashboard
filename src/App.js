8dd0958fff8dc8897140ffe2074138793b1998c9
James Taylor <jata20@student.bth.se>
2022-11-08 14:33:27 +0000

    Added routes for dashboard, map, cities, scooters and customers. Added sidebar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import {Route, Routes} from "react-router-dom";
+//import {   } from "./index";
+import { Dashboard, Map, Cities, Customers, Scooters } from "./pages";
+import { Sidebar } from "./components"
 
 function App() {
   return (
-    <div>
-      <p>ADMIN DASHBOARD</p>
-    </div>
+    <>
+      <div className="font-bold">
+        <p>ADMIN DASHBOARD</p>
+      </div>
+      <div>
+        <Sidebar/>
+      </div>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/map" element={<Map />}/>
+        <Route path="/cities" element={<Cities />}/>
+        <Route path="/scooters" element={<Scooters />}/>
+        <Route path="/customers" element={<Customers />}/>
+      </Routes>
+    </>
   );
 }
 
