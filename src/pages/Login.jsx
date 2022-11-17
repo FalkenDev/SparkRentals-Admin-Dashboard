345b058a00441889decb9803ab0cdf528f7b0648
James Taylor <jata20@student.bth.se>
2022-11-17 18:34:03 +0000

    Login page with background image and login form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { LoginForm } from "../components";
+import BlackLogoSpark from "../assets/img/transBGlogin.png";
 
-const Login = () => {
+const Login = ({ setToken }) => {
   return (
-    <div>Login</div>
-  )
-}
+    <div
+      style={{
+        backgroundImage: `url(${BlackLogoSpark})`,
+      }}
+      className="w-screen flex justify-center p-3 h-screen py-28"
+    >
+      <LoginForm setToken={setToken} />
+    </div>
+  );
+};
 
-export default Login
+export default Login;
