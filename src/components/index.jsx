45be923944770cb39d17e6bdc6ad022e5fe94ae0
James Taylor <jata20@student.bth.se>
2022-11-17 18:31:43 +0000

    Added login form to export

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,9 +1,12 @@
-export {default as Sidebar} from './Sidebar';
-export {default as Searchbar} from './Searchbar';
-export {default as CityList} from './Cities/CityList';
-export {default as RegisterCityForm} from './Cities/RegisterCityForm';
-export {default as CustomerList} from './Customers/CustomerList';
-export {default as Databox} from './Dashboard/Databox';
-export {default as Map} from './Map/Map';
-export {default as ScooterList} from './Scooters/ScooterList';
-export {default as RegisterScooterForm} from './Scooters/RegisterScooterForm';
+import { fromJSON } from "postcss";
+
+export { default as Sidebar } from "./Sidebar";
+export { default as Searchbar } from "./Searchbar";
+export { default as CityList } from "./Cities/CityList";
+export { default as RegisterCityForm } from "./Cities/RegisterCityForm";
+export { default as CustomerList } from "./Customers/CustomerList";
+export { default as Databox } from "./Dashboard/Databox";
+export { default as Map } from "./Map/Map";
+export { default as ScooterList } from "./Scooters/ScooterList";
+export { default as RegisterScooterForm } from "./Scooters/RegisterScooterForm";
+export { default as LoginForm } from "./Login/LoginForm";
