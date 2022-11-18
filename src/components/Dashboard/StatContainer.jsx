4fee9176f2dcc7fab0974d5d0e81bd4b8d0190e7
James Taylor <jata20@student.bth.se>
2022-11-18 16:00:59 +0000

    Component for individuall data pieces

---
+++ src/components/Dashboard/StatContainer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+const StatContainer = ({ icon, title, color }) => {
+  return (
+    <div
+      className="flex flex-row w-80 justify-between
+     bg-white p-4 rounded-xl h-32 align-middle
+       items-center"
+    >
+      <div
+        style={{ backgroundColor: color }}
+        className="p-3 rounded-2xl w-16 h-16 text-4xl text-white
+         flex justify-center align-middle"
+      >
+        {icon}
+      </div>
+      <div className="flex justify-between flex-col">
+        <h1 className="text-2xl font-semibold">9999</h1>
+        <p className="text-gray-600 font-semibold">{title}</p>
+      </div>
+    </div>
+  );
+};
+
+export default StatContainer;
