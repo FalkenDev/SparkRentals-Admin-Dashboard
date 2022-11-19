98e1502c629da9023fc5d5ceebe95260eb8a9ccc
James Taylor <jata20@student.bth.se>
2022-11-19 01:48:48 +0000

    Added DataLists component

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import { Databox } from "../components";
+import { Databox, DataLists } from "../components";
 const Dashboard = () => {
   return (
     <div className="w-full px-5">
       <h1 className="text-xl font-semibold py-2">Dashboard</h1>
       <div className="w-full flex justify-center relative ">
         <Databox />
+      </div>
+      <div>
+        <DataLists />
       </div>
     </div>
   );
