9804e4f474dd3ca6edc2af8cce646a7d80909543
James Taylor <jata20@student.bth.se>
2022-11-21 19:23:11 +0000

    Created map component capable of displaying zones and markers

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useRef, useState, useEffect } from "react";
+import "../../Map.css";
+import MapContext from "../../contexts/MapContext";
+import * as ol from "ol";
 
-const Map = () => {
+const Map = ({ children, zoom, center }) => {
+  const mapRef = useRef();
+  const [map, setMap] = useState(null);
+
+  // on component mount
+  useEffect(() => {
+    let options = {
+      view: new ol.View({ zoom, center }),
+      layers: [],
+      controls: [],
+      overlays: [],
+      //interactions: ol.interaction.defaults({ mouseWheelZoom: false }),
+    };
+
+    let mapObject = new ol.Map(options);
+    mapObject.setTarget(mapRef.current);
+    setMap(mapObject);
+
+    return () => mapObject.setTarget(undefined);
+  }, []);
+
+  // zoom change handler
+  useEffect(() => {
+    if (!map) return;
+
+    map.getView().setZoom(zoom);
+  }, [zoom]);
+
+  // center change handler
+  useEffect(() => {
+    if (!map) return;
+
+    map.getView().setCenter(center);
+  }, [center]);
+
   return (
-    <div>Map</div>
-  )
-}
+    <MapContext.Provider value={{ map }}>
+      <div ref={mapRef} className="ol-map border-2 border-slate-400">
+        {children}
+      </div>
+    </MapContext.Provider>
+  );
+};
 
-export default Map
+export default Map;
