45735b90f1f0931c54c5a69894326b79db931cc1
James Taylor <jata20@student.bth.se>
2022-11-21 21:20:01 +0000

    Updated

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { ContextProvider } from "./contexts/ContextProvider";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <ContextProvider>
     <App />
-  </ContextProvider>,
-  document.getElementById("root")
+  </ContextProvider>
 );
