996a9c1829152bdeaeaf8a215cbd4007bc5b308c
James Taylor <jata20@student.bth.se>
2022-11-28 18:06:37 +0000

    Minor changes to original searchbar

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,10 +1,26 @@
 import React from "react";
+import { useEffect } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
 
 const Search = ({ handleSearch, searchPhrase, setSearchPhrase }) => {
+  useEffect(() => {
+    const keyDownHandler = (event) => {
+      if (event.key === "Enter") {
+        event.preventDefault();
+        handleSearch();
+      }
+    };
+
+    document.addEventListener("keydown", keyDownHandler);
+
+    return () => {
+      document.removeEventListener("keydown", keyDownHandler);
+    };
+  }, []);
+
   return (
     <div className="flex row content-center p-4">
-      <div className="flex rounded-xl bg-orange-500 hover:bg-orange-600 w-96">
+      <div className="flex rounded-xl bg-blue-500 hover:bg-blue-600 w-96 transition-colors">
         <button onClick={handleSearch} className="flex items-center">
           <AiOutlineSearch className="w-5 h-5 mx-2 text-white" />
         </button>
