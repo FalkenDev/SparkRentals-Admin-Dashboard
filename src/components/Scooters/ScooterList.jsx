933bb08c0bd9f3536bd50a93659a502ca1fc094e
James Taylor <jata20@student.bth.se>
2022-11-29 16:38:08 +0000

    Edit layout and style of the scooter list table

--- src/components/Scooters/ScooterList.jsx
+++ src/components/Scooters/ScooterList.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { scooterData } from "../../data/mock/mockdata";
+import { Link, NavLink } from "react-router-dom";
 import { AiOutlineRight } from "react-icons/ai";
 
 const ScooterList = ({ filterPhrase }) => {
@@ -24,15 +25,17 @@ const ScooterList = ({ filterPhrase }) => {
       })
       .map((item, index) => {
         return (
-          <tr key={index} className="border-b text-base border-gray-700">
+          <tr key={index} className="border-b text-base border-gray-400">
             <td className="py-3 px-6">{index}</td>
             <td className="py-3 px-6">{item.owner}</td>
             <td className="py-3 px-6">{item.status}</td>
             <td className="py-3 px-6">{item.battery}%</td>
-            <td className="py-3 px-6">
-              <button>
-                <AiOutlineRight />
-              </button>
+            <td className="py-3 px-6 w-2">
+              <NavLink to={"/scooters/select"}>
+                <span>
+                  <AiOutlineRight />
+                </span>
+              </NavLink>
             </td>
           </tr>
         );
@@ -40,14 +43,15 @@ const ScooterList = ({ filterPhrase }) => {
   };
   return (
     <>
-      <table className="w-full text-lg table-auto text-left">
-        <thead className="text-lg text-gray-800 p-1 border-y border-gray-800">
+      <table className="w-full text-lg text-left content-between">
+        <thead className="uppercase bg-sidebarBlue text-gray-200">
           <tr>
             <th className="font-normal px-6">ID</th>
             <th className="font-normal px-6">City</th>
             <th className="font-normal px-6"> Status</th>
             {/* <th className="font-normal ">Current Position</th> */}
             <th className="font-normal px-6">Battery</th>
+            <th className="font-normal px-6">View</th>
           </tr>
         </thead>
         <tbody>{scooters()}</tbody>
