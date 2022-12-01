b582a17aaf994a0d17ef37db801914f4baec7891
James Taylor <jata20@student.bth.se>
2022-12-01 21:57:14 +0000

    Removed unused function for previous map

--- src/utils/maputils.js
+++ src/utils/maputils.js
@@ -9,24 +9,5 @@ const maputils = {
     });
     return Arr;
   },
-
-  addMarkers: function addMarkers(lonLatArray) {
-    const iconStyle = new Style({
-      image: new Icon({
-        anchorXUnits: "fraction",
-        anchorYUnits: "pixels",
-        anchor: [0.5, 1],
-        src: mapConfig.markerImage32,
-      }),
-    });
-    const features = lonLatArray.map((item) => {
-      let feature = new Feature({
-        geometry: new Point(fromLonLat(item)),
-      });
-      feature.setStyle(iconStyle);
-      return feature;
-    });
-    return features;
-  },
 };
 export default maputils;
