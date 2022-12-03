20a9b2b94914e8f771922d356a15436053f2502d
James Taylor <jata20@student.bth.se>
2022-12-03 15:53:02 +0000

    Added function for posting scooters

--- src/models/scooters.js
+++ src/models/scooters.js
@@ -1,4 +1,5 @@
 import config from "../config/config.json";
+import storage from "./storage";
 const scooter = {
   getScooters: async function getScooters() {
     const response = await fetch(
@@ -19,6 +20,34 @@ const scooter = {
       `${config.url}/scooters/${id}?api_key=${process.env.REACT_APP_REST_API_KEY}`
     );
     return response.json();
+  },
+
+  getScootersByCity: async function getScootersByCity(city) {
+    const response = await fetch(
+      `${config.url}/scooters/owner/${city}?api_key=${process.env.REACT_APP_REST_API_KEY}`
+    );
+    return response.json();
+  },
+
+  addScooter: async function addScooter(scooter) {
+    const data = {
+      owner: scooter.owner,
+      longitude: scooter.longitude,
+      latitude: scooter.latitude,
+      battery: scooter.battery,
+      status: scooter.status,
+      api_key: process.env.REACT_APP_REST_API_KEY,
+    };
+    const tokenObj = storage.readToken();
+    const response = await fetch(`${config.url}/scooters`, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "content-type": "application/json",
+        "x-access-token": tokenObj.token,
+      },
+    });
+    await response.json();
   },
 };
 
