b1a67f7f8496f3a4c2bcc0519122ed00c7385509
James Taylor <jata20@student.bth.se>
2022-12-05 18:37:33 +0000

    This component now handles filtering coordinates aswell as new popup feature and added zones from api

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -10,48 +10,115 @@ import {
 import * as L from "leaflet";
 import ScooterIcon from "../../assets/icons/ScooterMarker.svg";
 import "../../Map.css";
-import { areas } from "../../data/mock/mockdata";
-//import { Icon } from "leaflet";
+import scooterutils from "../../utils/scooterutils";
 
 //import * as parkData from "./data/skateboard-parks.json";
 
-const Map = ({ center, zoom, features, markers }) => {
-  //console.log(ScooterIcon);
+const Map = ({ center, zoom, features, scooters, cities }) => {
   const LeafIcon = L.Icon.extend({
     options: {},
   });
 
   const scooterIcon = new LeafIcon({
     iconUrl: ScooterIcon,
-    //iconSize: [38, 95],
-    iconAnchor: [22, 94],
+    iconSize: [68, 95],
+    iconAnchor: [34, 95],
     popupAnchor: [-3, -76],
-    //shadowUrl: "my-icon-shadow.png",
-    // shadowSize: [68, 95],
-    //shadowAnchor: [22, 94],
   });
+
+  const areaStyle = (type) => {
+    switch (type) {
+      case "parkingZone":
+        return {
+          fillColor: "blue",
+          fillOpacity: 0.1,
+        };
+      case "noParkingZone":
+        return {
+          fillColor: "red",
+          fillOpacity: 0.4,
+          weight: 1,
+        };
+      case "bonusParkingZone":
+        return {
+          fillColor: "green",
+          fillOpacity: 0.4,
+          weight: 1,
+        };
+    }
+  };
 
   function Search() {
     const map = useMap();
     if (features) {
-      map.flyTo(features, map.getZoom());
+      map.flyTo(features, zoom);
+      //map.setZoom(10);
     }
 
     return null;
   }
 
   function MarkersDisplay() {
-    if (markers) {
-      return markers.map((item) => {
-        return <Marker position={item} icon={scooterIcon} />;
+    if (scooters) {
+      return scooters.map((item, index) => {
+        return (
+          <Marker
+            key={index}
+            position={[item.coordinates.latitude, item.coordinates.longitude]}
+            icon={scooterIcon}
+          >
+            <Popup>
+              <h1 className="text-xl">{item.name}</h1>
+              <div
+                className="text-md text-white w-fit py-1 px-2 rounded-xl my-1"
+                style={{ backgroundColor: scooterutils.sateColor(item.status) }}
+              >
+                {item.status}
+              </div>
+              <div className="px-1">
+                <span className="text-md">
+                  Owner: {item.owner}
+                  <br />
+                  Battery: {item.battery}%
+                  <br />
+                  Lat: {item.coordinates.latitude}
+                  <br />
+                  Lon: {item.coordinates.longitude}
+                </span>
+              </div>
+            </Popup>
+          </Marker>
+        );
       });
     }
   }
 
   function AreasDisplay() {
-    return areas.geoObjects.map((item) => {
-      return <GeoJSON data={item} />;
-    });
+    if (cities) {
+      return cities.map((item) => {
+        return item.zones.map((data, index) => {
+          let geoObject = {
+            type: "Feature",
+            properties: { zonetype: data.zoneType },
+            geometry: {
+              type: "Polygon",
+              coordinates: [data.coordinates],
+            },
+          };
+          return (
+            <GeoJSON
+              key={index}
+              data={geoObject}
+              style={areaStyle(data.zoneType)}
+            >
+              <Popup>
+                <h1 className="text-xl">{item.name}</h1>
+              </Popup>
+            </GeoJSON>
+          );
+        });
+      });
+    }
   }
 
   return (
