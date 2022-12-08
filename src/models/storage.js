982584c3743e141e00a88a82424e1579bf921295
James Taylor <jata20@student.bth.se>
2022-12-08 15:30:44 +0000

    Added storage for toke dates

--- src/models/storage.js
+++ src/models/storage.js
@@ -1,13 +1,21 @@
 const storage = {
   //WRONG
   storeToken: function storeToken(token) {
+    localStorage.setItem("tokenDate", new Date().getTime());
     localStorage.setItem("token", token);
   },
   readToken: function readToken() {
-    return localStorage.getItem("token");
+    const token = localStorage.getItem("token");
+    const tokenDate = localStorage.getItem("tokenDate");
+    const tokenObj = {
+      token: token,
+      date: tokenDate,
+    };
+    return tokenObj;
   },
   deleteToken: function readToken() {
     localStorage.removeItem("token");
+    localStorage.removeItem("tokenDate");
   },
 };
 
