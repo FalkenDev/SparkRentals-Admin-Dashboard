a9936a29687da505673cc7b4c127b69befb6f4a7
James Taylor <jata20@student.bth.se>
2022-12-12 16:59:18 +0000

    Added map to city register so user can se where their city will be placed

--- src/components/Cities/RegisterCityForm.jsx
+++ src/components/Cities/RegisterCityForm.jsx
@@ -1,23 +1,21 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { GrClose } from "react-icons/gr";
 import { cityform } from "../../data/data";
+import { Map } from "../../components";
 import cities from "../../models/cities";
+import getCoordinates from "../../models/nominatim";
+const RegisterCityForm = ({
+  handleForm,
+  handleRegister,
+  setNewCity,
+  newCity,
+}) => {
+  const [coords, setCoords] = useState([0, 0]);
 
-const RegisterCityForm = ({ handleForm, cityData }) => {
-  const [newCity, setNewCity] = useState({
-    name: "",
-    fixedRate: 0,
-    timeRate: 0,
-    bonusParkingZoneRate: 0,
-    parkingZoneRate: 0,
-    noParkingZoneRate: 0,
-    cityChargingZoneRate: 0,
-    cityNoParkingToValidParking: 0,
-  });
-
-  const handleRegister = () => {
-    cities.addCity(newCity);
+  const getCoords = async () => {
+    const res = await getCoordinates(newCity.name);
+    setCoords([res.latitude, res.longitude]);
   };
 
   const handleFormData = (e) => {
@@ -46,7 +44,7 @@ const RegisterCityForm = ({ handleForm, cityData }) => {
     });
   };
   return (
-    <div className="bg-white p-4 reounded rounded-xl shadow-lg">
+    <div className="bg-white p-4 reounded rounded-xl shadow-lg w-128">
       <button
         onClick={handleForm}
         className="rounded-full transition-colors p-2 shadow-md hover:bg-red-400"
@@ -54,21 +52,44 @@ const RegisterCityForm = ({ handleForm, cityData }) => {
         <GrClose />
       </button>
       <h1 className="text-xl font-semibold text-center pb-2">Register City</h1>
-      <form id="newScooter" className="p-2 border-t border-gray-400 h-fit">
-        {allFields()}
-      </form>
-      <div className="text-center w-full">
-        <button
-          onClick={() => {
-            handleRegister();
-            handleForm();
-          }}
-          className="
+      <div className="flex flex-row justify-between border-t border-gray-400 p-4">
+        <form id="newScooter" className="p-2 h-fit">
+          {allFields()}
+          <div className="text-center w-full pt-3">
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                handleRegister(newCity);
+                handleForm();
+              }}
+              className="
       py-2 px-4 transition-colors bg-sidebarHover
     hover:bg-sidebarBlue text-white rounded-full"
-        >
-          Register
-        </button>
+            >
+              Register
+            </button>
+          </div>
+        </form>
+        <div className=" w-96 h-130 overflow-hidden flex flex-col pt-12">
+          <Map center={coords} features={coords} zoom={10} />
+          <div className="w-full">
+            <div className="text-xl mb-5 bg-slate-700 text-white py-1 px-3">
+              <p>Lat: {coords[0]} </p>
+              <p>Lon: {coords[1]}</p>
+            </div>
+            <div className="text-center">
+              <button
+                className="py-2 px-4 transition-colors bg-sidebarHover
+              hover:bg-sidebarBlue text-white rounded-full"
+                onClick={() => {
+                  getCoords();
+                }}
+              >
+                Get Coordinates
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
