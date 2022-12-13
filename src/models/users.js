b4ed3002353670c05a96561aad14786620aab580
James Taylor <jata20@student.bth.se>
2022-12-13 15:47:47 +0000

    Fixed methods for users update and delete

--- src/models/users.js
+++ src/models/users.js
@@ -1,9 +1,45 @@
+import storage from "./storage";
 const users = {
   getUsers: async function getScooters() {
     const response = await fetch(
       `${process.env.REACT_APP_API_URL}/users?api_key=${process.env.REACT_APP_REST_API_KEY}`
     );
     return response.json();
+  },
+  editUsers: async function editUsers(user) {
+    const data = {
+      user_id: user._id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      phoneNumber: user.phoneNumber,
+      email: user.email,
+      balance: user.balance,
+      api_key: process.env.REACT_APP_REST_API_KEY,
+    };
+    const tokenObj = storage.readToken();
+    await fetch(`${process.env.REACT_APP_API_URL}/users`, {
+      method: "PUT",
+      body: JSON.stringify(data),
+      headers: {
+        "content-type": "application/json",
+        "x-access-token": tokenObj.token,
+      },
+    });
+  },
+  deleteUsers: async function deleteUsers(userID) {
+    const data = {
+      user_id: userID,
+      api_key: process.env.REACT_APP_REST_API_KEY,
+    };
+    const tokenObj = storage.readToken();
+    await fetch(`${process.env.REACT_APP_API_URL}/users`, {
+      method: "DELETE",
+      body: JSON.stringify(data),
+      headers: {
+        "content-type": "application/json",
+        "x-access-token": tokenObj.token,
+      },
+    });
   },
 };
 
