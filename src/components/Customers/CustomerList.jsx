8322f269cb5e1561495bf5e018350df0116601e6
James Taylor <jata20@student.bth.se>
2022-12-13 15:49:20 +0000

    Button functions

--- src/components/Customers/CustomerList.jsx
+++ src/components/Customers/CustomerList.jsx
@@ -1,12 +1,29 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { useState } from "react";
-import { AiOutlineRight } from "react-icons/ai";
-const CustomerList = ({ filterPhrase, userData }) => {
+import { useState, useEffect } from "react";
+import { AiOutlineRight, AiOutlineCheck, AiOutlineClose } from "react-icons/ai";
+const CustomerList = ({
+  filterPhrase,
+  userData,
+  handleForm,
+  setLogData,
+  handleEditForm,
+  setSelectedUser,
+  handleRemoveAccount,
+  selectedUser,
+}) => {
   const [isOpen, setIsOpen] = useState({});
+  const [isDelete, setIsDelete] = useState({});
+
+  // useEffect(() => {
+  //   handleForm();
+  // }, [selectedUser]);
 
   const handleClick = (id) => {
     setIsOpen((prevState) => ({ ...prevState, [id]: !prevState[id] }));
+  };
+
+  const handleDeleteButton = (id) => {
+    setIsDelete((prevState) => ({ ...prevState, [id]: !prevState[id] }));
   };
 
   const rotate = (bool) => {
@@ -16,66 +33,106 @@ const CustomerList = ({ filterPhrase, userData }) => {
     return "rotate(0deg)";
   };
 
+  if (!userData) {
+    return <div>Loading</div>;
+  }
+
   const customerData = (user) => {
     return (
       <div className="w-full flex justify-between">
         <div className="p-3">
-          <form className="flex flex-row ">
+          <div className="flex flex-row ">
             <div className="flex flex-col px-2">
-              <label>First Name</label>
-              <input
-                className="border-b bg-gray-200 border-gray-800 mr-2 mb-3"
-                value={user.firstName}
-              />
-              <label>Last Name</label>
-              <input
-                className="border-b bg-gray-200 border-gray-800 mr-2"
-                value={user.lastName}
-              />
+              <h2>First Name</h2>
+              <p className="border-b bg-gray-200 border-gray-800 mr-2 mb-3 w-52">
+                {user.firstName}
+              </p>
+              <h2>Last Name</h2>
+              <p className="border-b bg-gray-200 border-gray-800 mr-2 w-52">
+                {user.lastName}
+              </p>
             </div>
             <div className="flex flex-col px-2">
-              <label>Email</label>
-              <input
-                className="border-b bg-gray-200 border-gray-800 mr-2 mb-3"
-                value={user.email}
-              />
-              <label>Phone Number</label>
-              <input
-                className="border-b bg-gray-200 border-gray-800 mr-2"
-                value={user.phoneNumber}
-              />
+              <h2>Email</h2>
+              <p className="border-b bg-gray-200 border-gray-800 mr-2 mb-3 w-52">
+                {user.email}
+              </p>
+              <h2>Phone Number</h2>
+              <p className="border-b bg-gray-200 border-gray-800 mr-2 w-52">
+                {user.phoneNumber}
+              </p>
             </div>
             <div className="flex flex-col px-2 ">
-              <label>Balance</label>
-              <input
-                className="border-b bg-gray-200 border-gray-800 mr-2"
-                value={user.balance + "kr"}
-              />
+              <h2>Balance</h2>
+              <p className="border-b bg-gray-200 border-gray-800 w-52 mr-2">
+                {user.balance + "kr"}
+              </p>
             </div>
-          </form>
+          </div>
         </div>
+
         <div className="flex flex-col p-4">
           <button
-            className="
-                my-1 py-1 px-3 transition-colors bg-sidebarHover w-36
-               hover:bg-sidebarBlue text-white rounded-full"
-          >
-            Edit
-          </button>
-          <button
-            className="
-                my-1 py-1 px-3 transition-colors bg-sidebarHover w-36
-               hover:bg-sidebarBlue text-white rounded-full"
-          >
-            Delete
-          </button>
-          <button
+            onClick={() => {
+              handleForm();
+              setLogData(user.history);
+            }}
             className="
                 my-1 py-1 px-3 transition-colors bg-sidebarHover w-36
                hover:bg-sidebarBlue text-white rounded-full"
           >
             Logs
           </button>
+          <button
+            onClick={() => {
+              setSelectedUser(user);
+              handleEditForm();
+            }}
+            className="
+                my-1 py-1 px-3 transition-colors bg-sidebarHover w-36
+               hover:bg-sidebarBlue text-white rounded-full"
+          >
+            Edit
+          </button>
+          {isDelete[user._id] ? (
+            <div>
+              <p>Confirm deletion?</p>
+              <div className="flex justify-evenly">
+                <button
+                  onClick={() => {
+                    handleDeleteButton(user._id);
+                    handleRemoveAccount(user._id);
+                  }}
+                  className="
+                my-1 p-1 text-2xl transition-colors bg-sidebarHover
+               hover:bg-sidebarBlue text-white rounded-full"
+                >
+                  <AiOutlineCheck />
+                </button>
+                <button
+                  onClick={() => {
+                    handleDeleteButton(user._id);
+                  }}
+                  className="
+                my-1 p-1 text-2xl transition-colors bg-sidebarHover 
+               hover:bg-sidebarBlue text-white rounded-full"
+                >
+                  <AiOutlineClose />
+                </button>
+              </div>
+            </div>
+          ) : (
+            <button
+              onClick={() => {
+                handleDeleteButton(user._id);
+              }}
+              className="
+                my-1 py-1 px-3 transition-colors bg-sidebarHover w-36
+               hover:bg-sidebarBlue text-white rounded-full"
+            >
+              Delete
+            </button>
+          )}
         </div>
       </div>
     );
@@ -108,16 +165,16 @@ const CustomerList = ({ filterPhrase, userData }) => {
               <td className="py-3 px-6 w-2">
                 <button
                   className="transition-transform"
-                  style={{ transform: rotate(isOpen[item._id.$oid]) }}
+                  style={{ transform: rotate(isOpen[item._id]) }}
                   onClick={() => {
-                    handleClick(item._id.$oid);
+                    handleClick(item._id);
                   }}
                 >
                   <AiOutlineRight />
                 </button>
               </td>
             </tr>
-            {isOpen[item._id.$oid] ? (
+            {isOpen[item._id] ? (
               <td colspan="6">
                 <div className="bg-gray-200 w-full border-b border-gray-400">
                   {customerData(item)}
