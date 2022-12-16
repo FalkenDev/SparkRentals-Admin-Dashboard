25408396f36d4ca54107ab984bf89bf5258999a6
James Taylor <jata20@student.bth.se>
2022-12-16 23:22:15 +0000

    Added router wrapper

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { ContextProvider } from "./contexts/ContextProvider";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <ContextProvider>
-    <App />
-  </ContextProvider>
+  <BrowserRouter>
+    <ContextProvider>
+      <App />
+    </ContextProvider>
+  </BrowserRouter>
 );
